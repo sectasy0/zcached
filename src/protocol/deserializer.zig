c181530d56a8f522ee997dd7cb1b44fa1f4a87ed
sectasy0 <sectasy0@gmail.com>
2024-01-01 21:19:27 +0100

    refactor: remove debug statement

--- src/protocol/deserializer.zig
+++ src/protocol/deserializer.zig
@@ -288,7 +288,6 @@ test "serialize map" {
     var value = types.AnyType{ .map = map };
 
     var result = try deserializer.process(value);
-    std.debug.print("result: {s}\n", .{result});
     // it's not guaranteed that the order of the map will be the same
     const expected = "%3\r\n+b\r\n$6\r\nsecond\r\n+a\r\n$5\r\nfirst\r\n+c\r\n$5\r\nthird\r\n";
     try std.testing.expectEqualStrings(expected, result);
