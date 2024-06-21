309d3af2d829b13669f1c9c08b68bf481c0421a7
xXenvy <quentisxd@gmail.com>
2024-06-21 15:30:39 +0200

    refactor(logger): revert changes from #6

--- src/server/processing/errors.zig
+++ src/server/processing/errors.zig
@@ -21,7 +21,7 @@ pub fn build_args(command_set: *const std.ArrayList(ZType)) Args {
 }
 
 // stream is a std.net.Stream
-pub fn handle(stream: anytype, err: anyerror, args: Args, logger: *Logger) !void {
+pub fn handle(stream: anytype, err: anyerror, args: Args, logger: *const Logger) !void {
     const out = stream.writer();
 
     logger.log(.Debug, "handling error: {}", .{err});
